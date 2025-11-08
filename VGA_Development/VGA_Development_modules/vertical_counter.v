0d3e64541df4f971f4f111d7f01a3c6f2188f094
William Grefaldeo <William.Grefaldeo01@student.csulb.edu>
2025-11-07 17:16:42 -0800

    Modified vertical counter

--- VGA_Development/VGA_Development_modules/vertical_counter.v
+++ VGA_Development/VGA_Development_modules/vertical_counter.v
@@ -1,25 +1,22 @@
 `timescale 1ns / 1ps
 
 module vertical_counter#(
-  parameter V_TOTAL = 525
+  parameter integer V_TOTAL = 525
 )(
-  input clk_25MHz, 
-  input reset,
-  input enable_V_counter,
-  output reg [9:0] V_count_value = 10'd0
+  input  wire       clk_25MHz,
+  input  wire       reset,               // async high
+  input  wire       enable_V_counter,    // 1 when we should increment
+  output reg  [9:0] V_count_value = 10'd0
 );
-
-always@(posedge clk_25MHz or posedge reset) begin
-  // Keep counting until 525
-  if(reset)
+always @(posedge clk_25MHz or posedge reset) begin
+  if (reset) begin
     V_count_value <= 10'd0;
-
-  else if(enable_V_counter == 10'b1) begin
-      if(V_count_value == (V_TOTAL - 1))
-        V_count_value <= V_count_value + 10'd1;
-      else
-        V_count_value <= 10'd0;
+  end else if (enable_V_counter) begin
+    if (V_count_value == V_TOTAL - 1)
+      V_count_value <= 10'd0;
+    else
+      V_count_value <= V_count_value + 10'd1;
   end
+  // else: hold
 end
-
 endmodule
